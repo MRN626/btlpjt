5881751caf2244f1811cdf3aee1c034ae0dda6b3
ngalukiye maureen <ngalukiyemaureen@gmail.com>
2022-02-10 17:50:03 +0100

    first commit

---
+++ app/Views/infos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis officia vitae, amet odit temporibus corrupti. Laudantium pariatur consequuntur, ipsam delectus inventore earum quasi odit vitae? Iusto perspiciatis nemo amet molestias.
+    </h1>
+</body>
+</html>
