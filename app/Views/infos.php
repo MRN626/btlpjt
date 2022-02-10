c1260245f985fd637cca0cad6c435e866a0e7454
ngalukiye maureen <ngalukiyemaureen@gmail.com>
2022-02-10 18:18:54 +0100

    commit 2

--- app/Views/infos.php
+++ app/Views/infos.php
@@ -10,5 +10,8 @@
     <h1>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis officia vitae, amet odit temporibus corrupti. Laudantium pariatur consequuntur, ipsam delectus inventore earum quasi odit vitae? Iusto perspiciatis nemo amet molestias.
     </h1>
+    <h2>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto aspernatur veniam quos necessitatibus ab quas adipisci, consequuntur aperiam, minima tempora, dignissimos asperiores consequatur ipsa labore perferendis pariatur voluptate illum voluptatum?
+    </h2>
 </body>
 </html>
